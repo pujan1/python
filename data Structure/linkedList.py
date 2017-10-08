12168d2a20499d1deeaf0aa29da7269a4e0659c9
pujan <pujan3892@gmail.com>
2017-10-07 22:21:05 -0700

    linked list added

---
+++ data Structure/linkedList.py
@@ -0,0 +1,35 @@
+class node:
+	def __init__ (self, data = None, next = none):
+		self.data = data
+		self.next = next
+		
+	def getNext (self):
+		return self.next
+
+	def setNext (self, node)
+		self.next = node
+
+	def getData (self)
+		return self.data
+
+	def setData (self, data)
+		self.data = data
+
+
+class LinkedList:
+	def __init__ (self, root = None):
+		self.size = 0
+		self.root = root
+
+	def getSize (self):
+		return self.zise
+
+	def addNode(self, node):
+		new_node = node (d, self.root)
+		self.root = new_node
+		self.size += 1
+
+
+
+
+		
